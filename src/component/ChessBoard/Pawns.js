3261e10986eb015fc78d7d326e5716b74863e216
dmc <dchellapan@gmail.com>
2023-08-21 15:19:03 -1100

    testing

---
+++ src/component/ChessBoard/Pawns.js
@@ -0,0 +1,27 @@
+const WhitePawn = () =>
+{
+   return(
+      <div className='whitePawn'><img src= "pieces/pawn_w.png" alt='whitePawn'/></div>
+   )
+}
+
+const BlackPawn = () =>
+{
+   return(
+      <div className='blackPawn'><img src='pieces/pawn_b.png' alt='blackPawn'/></div>
+   )
+}
+
+const ENUM_PAWN_TYPE = 
+{
+   whitePawn: <WhitePawn/>,
+   blackPawn: <BlackPawn/>,
+};
+
+
+function Pawns({pawnType, image}) 
+{
+   return ENUM_PAWN_TYPE[pawnType]
+}
+
+export default Pawns; 
