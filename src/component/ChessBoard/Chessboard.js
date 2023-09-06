eaa0ad6a3a77aaa5bb301a6a99ad94096a951967
dmc <dchellapan@gmail.com>
2023-09-06 13:24:57 -0700

    testing pushing images

--- src/component/ChessBoard/Chessboard.js
+++ src/component/ChessBoard/Chessboard.js
@@ -5,44 +5,49 @@ import Pawns from './Pawns';
 
 function ChessBoard(props)
 {  
-    
-    let board = props.board; 
-
-    board = []; 
-
     const xCoordinates = ["a", "b", "c", "d", "e", "f", "g", "h", "0"];
 
     const yCoordinates = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
 
+    // const [coordinates, setCoordinates] = useState('a1');
+
     let index = 0 
 
+    const coordinatesBoard = []; 
 
-    for(let y = yCoordinates.length - 1; y > 0; y--)
+    const RenderCoordinates = () =>
     {
-    
-        for(let x = 0; x < xCoordinates.length - 1; x++)
-        {       
-            index++;
+        for(let y = yCoordinates.length - 1; y > 0; y--)
+        {
+            for(let x = 0; x < xCoordinates.length - 1; x++)
+            {       
+                index++;
 
-            const tileKey = `${xCoordinates[x]}${yCoordinates[y]}`;
+                const tileKey = `${xCoordinates[x]}${yCoordinates[y]}`;
                 
-            if(index % 2 === 1) 
-            {
-                board.push(<Tile image={"whitePawn"} number={1} key={tileKey}></Tile>);
-            }
-            else
-            {
-                board.push(<Tile image={"blackPawn"} number={2} key={tileKey}></Tile>);
+                if(index % 2 === 1) 
+                {
+                    coordinatesBoard.push(<Tile tileType={"whiteTile"} key={tileKey}></Tile>);
+                }
+                else
+                {
+                    coordinatesBoard.push(<Tile tileType={"aquaTile"} key={tileKey}><img src='pieces/pawn_b.png' alt='ok'/></Tile>);
               
+                }
             }
+            index--
+       
         }
-        index--
+
+        console.log(coordinatesBoard);
+        return coordinatesBoard; 
     }
     
 
-
     return(<div className="App">
-            {board}
+            {<RenderCoordinates/>}
+            {<Pawns pawnType={"whitePawn"}></Pawns>}
+            {<Pawns pawnType={"blackPawn"}></Pawns>}
             </div>)
     
 }
--- src/component/ChessBoard/Tile.js
+++ src/component/ChessBoard/Tile.js
@@ -1,41 +1,29 @@
 import './Tile.css';
   
-const pieces = 
+const WhiteTile = () =>
 {
-   "whitePawn": "pieces/pawn_w.png",
-   "blackPawn": "pieces/pawn_b.png",
-   "kingWhite": "pieces/pawn_b.png",
-   "kingBlack": "pieces/pawn_b.png",
-   "knightWhite": "pieces/pawn_b.png",
-   "knightBlack": "pieces/pawn_b.png",
-   "bishopWhite": "pieces/pawn_b.png",
-   "bishopBlack": "pieces/pawn_b.png",
-   "queenWhite": "pieces/pawn_b.png",
-   "queenBlack": "pieces/pawn_b.png",
-   "rookWhite": "pieces/pawn_b.png",
-   "rookBlack": "pieces/pawn_b.png",
+   return(
+      <div className='TileWhite'></div>
+   )
 }
 
-let image = null;
-let number = 0;  
-
-function Tile(props) 
+const AquaTile = () =>
 {
-   if(props.piece) image = pieces(props.piece);
-   number = props.number;
+   return(
+      <div className='TileAqua'></div>
+   )
+}
 
-   if(number % 2 === 0)
-   {
-      return(
-         <div className='TileAqua'>{image && <div style={{backgroundImage: `url(${image})`}} className='chess-piece'/>}</div>
-      )
-   }
-   else
-   {
-      return(
-         <div className='TileWhite'>{image && <div style={{backgroundImage: `url(${image})`}} className='chess-piece'/>}</div>
-      )
-   }
+const ENUM_TILE_TYPE = 
+{
+   whiteTile: <WhiteTile/>,
+   aquaTile: <AquaTile/>,
+};
+
+
+function Tile({tileType}) 
+{
+   return ENUM_TILE_TYPE[tileType]
 }
 
 export default Tile;
